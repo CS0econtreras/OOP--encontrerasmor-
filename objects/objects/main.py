b894c10cb276cb035ee674bfc635ec607156f9ea
Ezequiel Contreras <ezeq317@gmail.com>
2023-02-24 13:22:06 -0700

    update

--- objects/objects/main.py
+++ objects/objects/main.py
@@ -9,31 +9,50 @@ class main(object):
         self._poly_elements:str
         self._polygon_list:list[str] = []
         self._vertices_amount:int
-        self._vertex_pairs:list[tuple[int,int]] =[]
+        self._vertex_pairs:list[tuple[int,int]] = []
 
     def set_polygon_num(self):
-        
-        self._polygon_num = int(sys.stdin.readline())
+         self._polygon_num = int(sys.stdin.readline())
     
-    def get_polygon_num(self):
-        
+    def get_polygon_num(self) -> int:
         return self._polygon_num
+
+    def get_poly_list(self) -> list:
+        return self._polygon_list
+    
+    def get_vertices_amount(self) -> int:
+        return self._vertices_amount
+    
+    def get_vertex_pairs(self) -> list:
+        return self._vertex_pairs
+    
+    def new_polygon(self):
+        new_polygon = convex_polygon()
+        new_polygon.set_area(self.get_vertices_amount(), self.get_vertex_pairs())
+        print(new_polygon.get_area())
 
     def set_poly_elements(self):
         self._poly_elements = sys.stdin.readline()
         self._polygon_list = self._poly_elements.split()
         self._vertices_amount = int(self._polygon_list[0])
-    
-    def get_vertices_amount(self) -> int:
-        return self._vertices_amount
-    
-        
+        self._vertex_pairs = []
 
-    def main():
-        pass
+    def answer(self):
+        self.set_polygon_num()
+        for i in range(self.get_polygon_num()):
+            self.set_poly_elements()
+            for j in range(self.get_vertices_amount()):
+                temp_list = self.get_poly_list()
+                x = int(temp_list[(2*j)+1])
+                y = int(temp_list[(2*j)+2])
+                self._vertex_pairs.append((x,y))
+            temp_list = [] 
+            self.new_polygon()   
 
-    
-    
+    @staticmethod
+    def main() -> None:
+        obj = main()
+        obj.answer()
 
-
-
+if __name__ == "__main__":
+    main.main()
--- objects/objects/polygonClass.py
+++ objects/objects/polygonClass.py
@@ -1,18 +1,24 @@
-
-
-class convex_polygon():
+class convex_polygon(object):
     
     def __init__(self):
-
         self._vertices_amount:int = 0
-        self._vertex_pairs:list[tuple[int,int]] =[]
+        self._vertex_pairs:list[tuple[int,int]] = []
+        self.n1: float = 0.0
+        self.n2: float = 0.0
+        self.n3: float = 0.0
 
-    def get_vertices_amount(self) -> int:
-        return self._vertices_amount
+    def set_area(self, vert_count, vert_pairs):
+        self.__init__()
+        self._vertex_pairs = vert_pairs
+        self._vertices_amount = vert_count
 
-    def get_area(self, vertices_amount, vertex_pairs):
-        pass
+        j = self._vertices_amount - 1
+        for i in range(0, self._vertices_amount):
+            self.n1 = (self._vertex_pairs[j][0] + self._vertex_pairs[i][0])
+            self.n2 = (self._vertex_pairs[j][1] - self._vertex_pairs[i][1])
+            self.n3 += (self.n1 * self.n2)
+            j = i
+        self.n3 = abs(self.n3 / 2)
 
-
-
-
+    def get_area(self) -> float:
+        return self.n3
